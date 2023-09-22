28d6c6f7c3898a97d51e5c812f9616a9f479c31f
Ziano <Ziano@ZIA>
2023-09-22 18:26:21 +0200

    la till fel hantering

--- Program.cs
+++ Program.cs
@@ -10,13 +10,21 @@ namespace ChessBoard
         {
             
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Mata in en siffra"); //asking user for a number
-            int input = int.Parse(Console.ReadLine()); // convert the string provided by the user to integer 
-
-
-            for (int row = 1; row <= input; row++) // this loop represents each row
+           
+            int number;
+            while(true)
             {
-                for (int column = 1; column <= input; column++) //  this loop represents each column
+                Console.WriteLine("Mata in en siffra"); //asking user for a number
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    break;
+                }
+                Console.WriteLine("Ogiltigt inmatning");
+            }
+
+            for (int row = 1; row <= number; row++) // this loop represents each row
+            {
+                for (int column = 1; column <= number; column++) //  this loop represents each column
                 {
                     if ((row + column) % 2 == 0) // in a division by 2 see if there is remaining value of row + column
                     {
